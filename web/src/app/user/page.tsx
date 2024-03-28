55fd3f0a8d02f073802ee7a5f95bd961f45abed0
gdpnei2002 <joseneilson25@gmail.com>
2024-03-28 18:41:06 -0300

    pagina de perfil

--- web/src/app/user/page.tsx
+++ web/src/app/user/page.tsx
@@ -1,19 +1,32 @@
-import { getUser } from "@/lib/auth";
+import { api } from "@/lib/api";
 import Image from "next/image";
+import { cookies } from "next/headers";
+import { getUser } from "@/lib/auth";
 
-export default function User() {
-  const { name, avatarUrl } = getUser();
+export default async function User() {
+  const { name, avatarUrl, sub } = getUser();
+  const token = cookies().get("token")?.value;
+
+  const user = await api.get(`/users/${sub}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
   return (
-    <div className="flex items-center gap-3 text-left">
-      <Image
-        src={avatarUrl}
-        width={40}
-        height={40}
-        alt=""
-        className="h-10 w-10 rounded-full"
-      />
+    <div>
+      <div className="flex items-center gap-3 text-left">
+        <Image
+          src={avatarUrl}
+          width={40}
+          height={40}
+          alt=""
+          className="h-10 w-10 rounded-full"
+        />
 
-      <p className="max-w-[140px] text-sm leading-snug">{name}</p>
+        <p className="max-w-[140px] text-sm leading-snug">{name}</p>
+      </div>
+      <p className="text-sm leading-snug mt-3">{user.data.bio}</p>
     </div>
   );
 }
